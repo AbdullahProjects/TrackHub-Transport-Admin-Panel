c365bc28f4dc122c62a965413d43596e40ad5a94
Abdullah Khan Kakar <2022bbit71@student.uet.edu.pk>
2025-11-17 20:19:35 +0500

    disable button during task execution

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -11,7 +11,8 @@ const AppButton = ({
     <button
       type={isSubmitButton ? "submit" : "button"}
       onClick={isLoading ? null : onTap}
-      className={`px-8 py-2.5 rounded-md ${isLoading? "bg-gray-400" : "bg-primary"} text-white text-[15px] font-medium hover:cursor-pointer hover:bg-primary/90 hover:shadow-md transition-all duration-300 active:ring-2 ring-black/10 ${
+      disabled={isLoading}
+      className={`px-8 py-2.5 rounded-md disabled:bg-gray-400 bg-primary text-white text-[15px] font-medium hover:cursor-pointer hover:bg-primary/90 hover:shadow-md transition-all duration-300 active:ring-2 ring-black/10 ${
         fullWidth ? "w-full" : ""
       }`}
     >
